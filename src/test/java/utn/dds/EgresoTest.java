430daa9db868fdfab96a7280c7a6348bb13ed7ce
Magali Modugno <magalimodugno@gmail.com>
2020-04-05 13:32:23 -0300

    Tests realizados

--- src/test/java/utn/dds/EgresoTest.java
+++ src/test/java/utn/dds/EgresoTest.java
@@ -1,12 +1,40 @@
 package utn.dds;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class EgresoTest {
-
+	private Egreso egreso;
+	private Articulo computadora = new Articulo("Computadora",100);
+	private Articulo escritorio = new Articulo("Escritorio",50);
+	
+	
+	@Before
+	public void init() {
+		    egreso = new Egreso();
+		  }
+	
 	@Test
-	public void testTotalDeRemitoSegunCompras() {
+	public void testPrecioTotalDeEgresos() {
 		
+		egreso.cargarItem(computadora);
+		egreso.cargarItem(escritorio);
+		
+		Assert.assertEquals(150, egreso.precioTotal(), 0.01);
 	}
+	
+	
+	@Test (	expected=RuntimeException.class)
+
+ 	public void testCerrarOperacionesYNoCambiarPrecios() {
+		egreso.cargarItem(computadora);
+		egreso.cerrarOperacion();
+		computadora.cambiarPrecio(200);
+ 	}
+	
 }
+
+
+ 
